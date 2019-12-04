f47052c18a39850bc5768af765d50245858d46bc
Nick-Lisi <nlisi1@yahoo.com>
2019-12-04 12:34:42 -0500

    Create server.js

---
+++ Final_Project/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+
+//app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.render("index");
+})
+
+app.listen(port, function(){
+    console.log("app running")
+})
